37df9930c561c58a114ff1e348b50f1440027e37
eggacheb <115781643+eggacheb@users.noreply.github.com>
2024-07-25 18:21:31 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.hostname = 'eggacheb-fast.hf.space'; // Use 'hostname' instead of 'host'
+    url.hostname = '128.204.223.70:17667'; // Use 'hostname' instead of 'host'
 
     // Create a new request with the modified URL
     const newRequest = new Request(url.toString(), {
